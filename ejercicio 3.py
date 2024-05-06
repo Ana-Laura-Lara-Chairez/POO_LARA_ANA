5daffe52dbe1dc80994cb95f5ab00fe1f0b3bad4
Ana Laura Lara Chairez <analauris2103@gmail.com>
2024-05-06 11:48:11 -0600

    ejercicios primera clase

---
+++ ejercicio 3.py
@@ -0,0 +1,8 @@
+print ("Ingresa el valor de la base: ")
+base = float(input())
+print ("Ingresa el valor de la altura: ")
+altura = float(input())
+
+area = base * altura
+
+print ("El área es: " , area)
